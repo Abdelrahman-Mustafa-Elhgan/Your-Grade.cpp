92a25c795ee72047dd9d4aff4957972afdc169b3
Abdelrahman Mustafa <abdalrahman.elmustafa@gmail.com>
2025-07-19 18:54:12 +0300

    Update Your Grade.cpp

--- Your Grade.cpp
+++ Your Grade.cpp
@@ -3,20 +3,27 @@ using namespace std;
 
 int main()
 {
-    int grade;
-    cout << "Enter Your Grade: ";
-    cin >> grade;
+int grade;
+cout << "Enter Your Grade: ";
+cin >> grade;
 
-    if (grade >= 90 && grade <= 100)
-        cout << "You got: A ";
-    else if (grade >= 80 && grade <= 90)
-        cout << "You got: B";
-    else if (grade >= 70 && grade <= 80)
-        cout << "You got: C";
-    else if (grade >= 60 && grade <= 70)
-        cout << "You got: D";
-    else if (grade < 60)
-        cout << "You got: F";
-    else
-        cout << "This Is not Found";
+if (grade < 0 || grade > 100) {
+	cout << "Bad input" << endl;
+}
+else if (grade >= 90) {
+	cout << "You got Excellent" << endl;
+}
+else if (grade >= 80) {
+	cout << "You got Very Good" << endl;
+}
+else if (grade >= 70) {
+	cout << "You got Good" << endl;
+}
+else if (grade >= 60) {
+	cout << "You got Acceptable" << endl;
+}
+else {
+	cout << "You got Failed" << endl;
+}
+
 }
